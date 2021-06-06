fb1826f90de50523286980024ce3da5aa105831d
Kawarabata <kwrbt0404@gmail.com>
2021-06-06 12:00:28 +0000

    create book model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :books
+
   validates :name, presence: true, uniqueness: true, length: { in: 2..20 }
   validates :introduction, length: { maximum: 50 }
 end
